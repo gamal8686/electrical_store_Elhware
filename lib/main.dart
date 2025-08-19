1879e142855d66db16dc9cbf48eb1b934f8ae784
Gamal Osama <gamal8686@gmail.com>
2025-08-19 13:51:21 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:electrical_store_app/constants.dart';
+import 'package:electrical_store_app/models/Home.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        title: Text(
+          'مرحبا بكم في متجر الهوارى',
+
+          style: GoogleFonts.getFont(
+            'Almarai',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))],
+      ),
+      body: Home(),
+    );
+  }
+}
---
+++ lib/models/Home.dart
@@ -0,0 +1,44 @@
+import 'package:electrical_store_app/models/prodectCard.dart';
+import 'package:electrical_store_app/models/product.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: 28),
+          Expanded(
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                ),
+                ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) =>
+                      prodectCard(vvv: products[index], Index: index),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
